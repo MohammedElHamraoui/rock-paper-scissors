a1b814de2d7b2e720ffc99425bc35d6c9533ab3b
Mohammed El Hamraoui <92893757+MohammedElHamraoui@users.noreply.github.com>
2025-01-30 08:27:02 +0100

    Write the logic to get the computer choice
    Create a new function named getComputerChoice.
    getComputerChoice will randomly return one of the following string
    values: “rock”, “paper” or “scissors”.

--- scrip.js
+++ scrip.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+function getComputerChoice() {
+  let computerChoice = Math.floor(Math.random() * 3);
+  switch (computerChoice) {
+    case 0:
+      return "Rock";
+    case 1:
+      return "Paper";
+    default:
+      return "Scissors";
+  }
+}
+
+let computerChoice = getComputerChoice();
